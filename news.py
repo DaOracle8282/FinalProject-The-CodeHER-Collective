b8de17a2555b89e32b0bb96f0ed368e84d8fe09e
Kayla Ricumstrict <kaylaricumstrict@Kaylas-MacBook-Pro-2.local>
2024-12-09 06:31:15 -0500

    Updated functions imported from spotify.py in main.py

--- news.py
+++ news.py
@@ -1,12 +1,1 @@
-import requests
-import sqlite3
-import os
-
-#Step 1: Connect to News API
-def connect_to_news_api():
-   pass
-
-#Step 2: Create articles table. 
-def create_articles_tables():
-    pass
 
