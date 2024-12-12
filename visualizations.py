dada85e421d1bdffbec9e2a5493ca98ca7604dd3
Kayla Ricumstrict <kaylaricumstrict@Kaylas-MacBook-Pro-2.local>
2024-12-11 19:45:11 -0500

    Completed longest_movie_soundtrack_chart function

--- visualizations.py
+++ visualizations.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 import matplotlib.pyplot as plt
 
 def genre_bar_chart(cur):
@@ -52,6 +53,40 @@ def genre_horizontal_bar_chart(cur):
     plt.show()
 
 
+def parse_duration(duration_str):
+    # Duration string is in "HH:MM:SS" format
+    hrs, mins, secs = map(int, duration_str.split(':'))
+    return datetime.timedelta(hours=hrs, minutes=mins, seconds=secs)
+
+def seconds_from_timedelta(td):
+    return td.total_seconds()
+
+def longest_movie_soundtracks_chart(cur): 
+    cur.execute("""SELECT soundtracks.movie_title, soundtracks.total_duration
+                FROM soundtracks
+                JOIN Movies ON soundtracks.movie_id =Movies.id
+                ORDER BY soundtracks.total_duration DESC
+                LIMIT 5""")
+    results = cur.fetchall()
+    movie_titles = [row[0] for row in results]
+    durations = [parse_duration(row[1]) for row in results]
+
+    # Convert timedelta to string format for display purposes
+    duration_strings = [str(duration) for duration in durations]
+    
+    # Prepare duration in seconds for plotting
+    duration_in_seconds = [seconds_from_timedelta(duration) for duration in durations]
+
+    fig, ax = plt.subplots(figsize=(12,8))
+    ax.bar(movie_titles, duration_in_seconds, color='green')
+    ax.set(xlabel="Movie Title", ylabel="Length (in Seconds)", title="2024 Movies(with Movie in the Title) with the Longest Soundtracks")
+    fig.autofmt_xdate(rotation=20)
+    fig.savefig("longest_movie_soundtracks.png")
+    plt.show()
+    
+
+
+
 def main():
     """
     Main function to generate visualizations.
@@ -62,6 +97,7 @@ def main():
     # Generate visualizations
     genre_bar_chart(cur)
     genre_horizontal_bar_chart(cur)
+    longest_movie_soundtracks_chart(cur)
 
     conn.close()
 
