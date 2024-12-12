1185a1635f6904d95ade75c1f3ed684ae779dce4
harshaalu <harshaluitel@gmail.com>
2024-12-12 13:17:58 -0500

    Changing API Key again, so the code can run

--- news.py
+++ news.py
@@ -5,7 +5,7 @@ import json
 import os
 
 # Constants
-API_KEY = "da38bc1769ff4702a467994222783ec5"
+API_KEY = "80dd3b4afaa84da7b3b9be836471ea07"
 BASE_URL = "https://newsapi.org/v2/everything"
 
 
@@ -32,6 +32,7 @@ def setup_articles_table(db_name):
                 published_date TEXT,
                 article_content TEXT,
                 UNIQUE(movie_title, article_title, published_date)
+                FOREIGN KEY (movie_id) REFERENCES Movies(id)
             )
         """
         )
